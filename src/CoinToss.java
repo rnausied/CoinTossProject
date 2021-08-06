44be41d3c579d55ef69d634d72c8e39668ea31d8
rnausied <rnausied@gmail.com>
2021-08-06 10:31:36 -0400

    a new message for Cointoss

---
+++ src/CoinToss.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class CoinToss{
+	
+	public int tossACoin(){
+		
+		Random rand = new Random();
+		
+		int toss = Math.abs(rand.nextInt()) % 2;
+		
+		return toss;
+		
+		
+	}
+	
+	public static void main(String args[]){
+		
+		CoinToss game = new CoinToss();
+		
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+		System.out.println(game.tossACoin());
+			
+			
+		}
+		
+	}
